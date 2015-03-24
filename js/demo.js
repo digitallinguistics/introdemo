326277ba453d0e9d8c8cc21d6395dca14f68ed16
Daniel W. Hieber <dwhieb@gmail.com>
2015-03-24 02:14:42 -0700

    phoneme count visualization

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,7 @@
 // Intro Demo
 
 var nodes = {
+  charCount: document.querySelector('#charCount'),
   transcriptionBox: document.querySelector('#transcriptionBox'),
   translationBox: document.querySelector('#translationBox'),
   wordsArea: document.querySelector('#inputArea .words')
@@ -9,8 +10,8 @@ var nodes = {
 var delimiters = [' ', '.', ',', '!', '?', ':', ';', '/', '\\', '[', '\\]', '{', '}', '<', '>', '-', '\u3000', '\u00A1', '\u00BF', '\u0022', '\u0027', '\u00AB', '\u00BB', '\u2018', '\u2019', '\u201A', '\u201B', '\u201C', '\u201D', '\u201E', '\u201F', '\u2039', '\u203A', '\u300C', '\u300D', '\u300E', '\u300F', '\u301D', '\u301E', '\u301F', '\uFE41', '\uFE42', '\uFE43', '\uFE44', '\uFF02', '\uFF07', '\uFF62', '\uFF63'];
 
 var phrase = {
-  transcription: null,
-  translation: null,
+  transcription: '',
+  translation: '',
   words: []
 };
 
@@ -45,7 +46,23 @@ function displayJSON() {
   displayViz();
 };
 
-function displayStats() {};
+function displayStats() {
+  charCount.innerHTML = '';
+  var uniqueChars = [];
+  var rawChars = phrase.transcription.split('');
+  
+  rawChars.forEach(function(chr) {
+    var some = uniqueChars.some(function(c) { return c == chr; });
+    if (!some) { uniqueChars.push(chr); }
+  });
+  
+  uniqueChars.forEach(function(chr) {
+    var count = rawChars.filter(function(c) { return c == chr; }).length;
+    var li = document.createElement('li');
+    li.textContent = chr + ' : ' + count;
+    charCount.appendChild(li);
+  });
+};
 
 function displayWords() {
   nodes.wordsArea.innerHTML = '';
@@ -64,7 +81,15 @@ function displayViz() {
   displayDict();
 };
 
-function download() {};
+function download() {
+  var file = new Blob([JSON.stringify(phrase, null, 2)], { type: 'application.json' })
+  var a = document.createElement('a');
+  var url = URL.createObjectURL(file);
+  a.download = 'myData.json';
+  a.href = url;
+  document.body.appendChild(a);
+  a.click();
+};
 
 function tokenize() {
   var regExp = new RegExp('[(' + delimiters.join(')(') + ')]+', 'g');
@@ -99,6 +124,7 @@ function update() {
 };
 
 // Event listeners
+document.querySelector('#downloadButton').addEventListener('click', download);
 nodes.transcriptionBox.addEventListener('input', update);
 nodes.translationBox.addEventListener('input', updatePhrase);
 nodes.wordsArea.addEventListener('change', updateWords);
